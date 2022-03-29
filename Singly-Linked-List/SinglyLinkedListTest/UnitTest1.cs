fd2ca6d70de2caf7028d5fc646f0d1e0214b849e
olashlool <olashlool94@gmail.com>
2022-03-29 23:50:56 +0300

    Linked List Insertions

--- Singly-Linked-List/SinglyLinkedListTest/UnitTest1.cs
+++ Singly-Linked-List/SinglyLinkedListTest/UnitTest1.cs
@@ -41,5 +41,81 @@ namespace SinglyLinkedListTest
             bool expected = false;
             Assert.Equal(expected, list.Includes(3));
         }
+        [Fact]
+        public void TestAppend()       // Test Add a node to the end of the linked list
+        {
+            LinkedList list = new LinkedList();
+            list.Insert(1);
+            list.Insert(3);
+            list.Insert(2);
+            Assert.Equal("[2] -> [3] -> [1] -> NULL", list.ToString());
+            list.Append(5);
+            Assert.Equal("[2] -> [3] -> [1] -> [5] -> NULL", list.ToString());
+        }
+        
+        [Fact]
+        public void TestMultipleAppend()       // Test Append multiple nodes to the end of a linked list
+        {
+            LinkedList list = new LinkedList();
+            list.Insert(1);
+            list.Insert(3);
+            list.Insert(2);
+            Assert.Equal("[2] -> [3] -> [1] -> NULL", list.ToString());
+            list.Append(20);
+            list.Append(5);
+            list.Append(21);
+            Assert.Equal("[2] -> [3] -> [1] -> [20] -> [5] -> [21] -> NULL", list.ToString());
+        }
+        [Fact]
+        public void TestInsertBefore()       // Test insert a node before a node located i the middle of a linked list
+        {
+            LinkedList list = new LinkedList();
+            list.Append(1);
+            list.Append(3);
+            list.Append(2);
+            Assert.Equal("[1] -> [3] -> [2] -> NULL", list.ToString());
+            list.InsertBefore(3, 5);
+            Assert.Equal("[1] -> [5] -> [3] -> [2] -> NULL", list.ToString());
+        }
+        
+        [Fact]
+        public void TestInsertBeforeFirstNode()       // Test insert a node before the first node of a linked list
+        {
+            LinkedList list = new LinkedList();
+            list.Append(1);
+            list.Append(3);
+            list.Append(2);
+            Assert.Equal("[1] -> [3] -> [2] -> NULL", list.ToString());
+            list.InsertBefore(1, 5);
+            Assert.Equal("[5] -> [1] -> [3] -> [2] -> NULL", list.ToString());
+        }
+
+        
+        [Fact]
+
+        public void TestInsertAfter()       // Test insert after a node in the middle of the linked list
+        {
+            LinkedList list = new LinkedList();
+            list.Append(1);
+            list.Append(3);
+            list.Append(2);
+            Assert.Equal("[1] -> [3] -> [2] -> NULL", list.ToString());
+            list.InsertAfter(3, 5);
+            Assert.Equal("[1] -> [3] -> [5] -> [2] -> NULL", list.ToString());
+        }
+
+        
+        [Fact]
+
+        public void TestInsertNodeAfterTheLastNode()      //Test insert a node after the last node of the linked list
+        {
+            LinkedList list = new LinkedList();
+            list.Append(1);
+            list.Append(3);
+            list.Append(2);
+            Assert.Equal("[1] -> [3] -> [2] -> NULL", list.ToString());
+            list.InsertAfter(2, 5);
+            Assert.Equal("[1] -> [3] -> [2] -> [5] -> NULL", list.ToString());
+        }
     }
 }
