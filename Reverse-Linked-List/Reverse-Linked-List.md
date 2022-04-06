d00946ae3a0a8422ab5c6e925a9b574a76ff73d9
olashlool <olashlool94@gmail.com>
2022-04-07 02:05:06 +0300

    Reverse Linked List

---
+++ Reverse-Linked-List/Reverse-Linked-List.md
@@ -0,0 +1,21 @@
+﻿# Reverse Linked List 
+
+## Description
+
+Write a method called Reverse() that reverse a singly linked list.
+
+
+
+## Whiteboard Process
+![Reverse - Whiteboard](Assest/Reverse.png)
+
+
+## Approach & Efficiency
+#### Reverse
+- Big O Time = O(n) 
+- Big O Space = O(1)
+
+
+The iterative approach to reversing the array remains linear because constants are not taken 
+into consideration. Space remains O(1) since no new data structures are created.
+
---
+++ Reverse-Linked-List/Reverse-Linked-List/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reverse_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Insert(2);
+            list.Insert(5);
+            list.Insert(8);
+            list.Insert(5);
+            list.Insert(10);
+            Console.WriteLine(list.ToString()) ;
+            Console.WriteLine("List after Reverse....");
+            list.Reverse();
+            Console.WriteLine(list.ToString());
+
+        }
+    }
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+        public Node(int value)
+        {
+            Data = value;
+            Next = null;
+        }
+    }
+}
